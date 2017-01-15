e19a33f64d2cddc70df030a1e69dc371a6cf0432
Wifsimster <wifsimster@gmail.com>
2017-01-15 15:06:12 +0100

    Change GPIO state at start

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ gpio2 = 4
 gpio.mode(gpio0, gpio.OUTPUT)
 -- GPIO2 CONTACT
 gpio.mode(gpio2, gpio.INPUT)
-gpio.write(gpio2, gpio.LOW)
+gpio.write(gpio2, gpio.HIGH)
 
 prev_value = 0
 
@@ -25,29 +25,34 @@ function sendData(value)
 end
 
 local cnt = 0
+val = 1
 
 -- Trying to connect to AP
-tmr.alarm(3, 1000, 1, function()
-        if (wifi.sta.getip() == nil) and (cnt < 20) then
+tmr.alarm(0, 1000, 1, function()
+    if (wifi.sta.getip() == nil) and (cnt < 20) then
+        gpio.write(gpio0, gpio.HIGH)
+        print("Trying Connect to AP, Waiting...")
+        cnt = cnt + 1
+        gpio.write(gpio0, gpio.LOW)
+    else
+        tmr.stop(0)
+        if (cnt < 60) then
+            print("Config done, IP is "..wifi.sta.getip())
             gpio.write(gpio0, gpio.HIGH)
-            print("Trying Connect to AP, Waiting...")
-            cnt = cnt + 1
-            gpio.write(gpio0, gpio.LOW)
+            
+            -- Send GPIO2 when value change
+            gpio.trig(gpio2, 'both', function()
+                if(gpio.read(gpio2) ~= val) then
+                    val = gpio.read(gpio2)
+                    sendData(val)
+                    print("Send: "..val)
+                end
+            end)
         else
-            tmr.stop(3)
-            if (cnt < 60) then
-                print("Config done, IP is "..wifi.sta.getip())
-                gpio.write(gpio0, gpio.HIGH)
-                -- Send GPIO2 when value change
-                gpio.trig(gpio2, 'both', function()
-                    print("Send: "..gpio.read(gpio2))
-                    sendData(gpio.read(gpio2))
-                end)
-            else
-                print("Wifi setup time more than 60s")
-                gpio.write(gpio0, gpio.LOW)
-            end
-            cnt = nil;
-            collectgarbage();
+            print("Wifi setup time more than 60s")
+            gpio.write(gpio0, gpio.LOW)
         end
-    end)
+        cnt = nil;
+        collectgarbage();
+    end
+end)
